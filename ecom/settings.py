3b666e97eb9ad40d9308882156022ec0cc8cf4ea
--global <utkarsh.kulsreshtha@gmail.com>
2023-02-22 15:19:52 +0530

    store and cart functionality

--- ecom/settings.py
+++ ecom/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'category',
     'accounts',
     'store',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -64,6 +65,8 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'category.context_processors.menu_links',
+                'cart.context_processors.counter',
+                
             ],
         },
     },
