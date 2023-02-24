aa4191dbc46d343d70fb7c8c29155700a8c43b49
utkarsh-kulsreshtha <utkarsh.kulsreshtha@gmail.com>
2023-02-24 13:57:50 +0530

    added requirement.txt

--- ecom/urls.py
+++ ecom/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('', views.home, name='home'),
                   path('store/', include('store.urls')),
-                  path('cart/',include('cart.urls')),
+                  path('cart/', include('cart.urls')),
+                  path('accounts/', include('accounts.urls')),
 
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
